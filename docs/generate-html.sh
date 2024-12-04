3a98b435f80604f5e1f3e59c877b6d9cedcd055e
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2024-12-04 23:04:57 +0000

    Add to generate-html.sh

--- docs/generate-html.sh
+++ docs/generate-html.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 pandoc --css=main.css --embed-resources --standalone --number-sections -o ./dist/readme.html ./readme.md
+wkhtmltopdf --default-header toc ./dist/readme.html ./dist/readme.pdf
